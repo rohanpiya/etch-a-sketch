3cb6692a2639a7b3d8f65cf93d337fa33a6acbff
rohanpiya <piyar@dickinson.edu>
2026-06-24 12:19:17 +0545

    Fix hovering logic

--- script.js
+++ script.js
@@ -6,24 +6,26 @@ starterButton.addEventListener('click', () => {
     gridSize = Number(gridSize);
 
     bigContainer.replaceChildren();
-    
-    for(let i = 0; i < gridSize; i++) {
-    const smallContainer = document.createElement('div');
-    smallContainer.classList.add('small-container');
-    for(let j = 0; j < gridSize; j++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        smallContainer.appendChild(square);
+
+    for (let i = 0; i < gridSize; i++) {
+        const smallContainer = document.createElement('div');
+        smallContainer.classList.add('small-container');
+        for (let j = 0; j < gridSize; j++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            smallContainer.appendChild(square);
+        }
+        bigContainer.appendChild(smallContainer);
+
+        const squares = document.querySelectorAll('.square');
+
+        squares.forEach(square => {
+            square.addEventListener('mouseenter', () => {
+                square.style.backgroundColor = "rgb(255, 55, 89)";
+            })
+        })
     }
-    bigContainer.appendChild(smallContainer);
-}
 });
 
-const squares = document.querySelectorAll('.square');
 
-squares.forEach(square => {
-    square.addEventListener('mouseenter', () => {
-        square.style.backgroundColor = "rgb(255, 55, 89)";
-    })
-})
 
