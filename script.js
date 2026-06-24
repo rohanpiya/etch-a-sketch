e4f314f9a06793027d12fefba093777ca576d328
rohanpiya <piyar@dickinson.edu>
2026-06-24 12:04:45 +0545

    Style using css instead of js

--- script.js
+++ script.js
@@ -6,12 +6,6 @@ for(let i = 0; i < 16; i++) {
     for(let j = 0; j < 16; j++) {
         const square = document.createElement('div');
         square.classList.add('square');
-        square.style.width = '6.25%';
-        square.style.paddingBottom = '6.25%';
-        square.style.height = '0';
-        square.style.backgroundColor = 'black';
-        square.style.border = 'white 1px solid';
-        square.style.margin = '0';
         smallContainer.appendChild(square);
     }
     bigContainer.appendChild(smallContainer);
