a84c1759e05e2b9404ab6bde5fddef4c8c040548
rohanpiya <piyar@dickinson.edu>
2026-06-24 12:17:44 +0545

    Add logic to the js such that the user is prompted for the gridsize

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 const bigContainer = document.querySelector('.big-container');
+const starterButton = document.querySelector('button');
 
-for(let i = 0; i < 16; i++) {
+starterButton.addEventListener('click', () => {
+    let gridSize = prompt("Please enter the grid size");
+    gridSize = Number(gridSize);
+
+    bigContainer.replaceChildren();
+    
+    for(let i = 0; i < gridSize; i++) {
     const smallContainer = document.createElement('div');
     smallContainer.classList.add('small-container');
-    for(let j = 0; j < 16; j++) {
+    for(let j = 0; j < gridSize; j++) {
         const square = document.createElement('div');
         square.classList.add('square');
         smallContainer.appendChild(square);
     }
     bigContainer.appendChild(smallContainer);
 }
+});
 
 const squares = document.querySelectorAll('.square');
 
